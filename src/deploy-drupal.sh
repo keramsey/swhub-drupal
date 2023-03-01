31060ca785b0673d54d40c62fc615c9cf8becba1
keramsey <kramsey@jornada-vmail.nmsu.edu>
2023-03-01 19:58:20 +0000

    Fix sed command

--- src/deploy-drupal.sh
+++ src/deploy-drupal.sh
@@ -151,7 +151,7 @@ my="[mysqldump${PROJECT}]\n"
 
 #  sed -e 's/^"//' -e 's/"$//' <<<"$var1"
 
-my+="user="$(sed \-e \'s\/\^\'\/\/\' -e \'s\/\\\'\$\/\/\' \<\<\<\$\{username\})"\n"
+my+="user="$(sed \'s\/\^\'\/\/\' \'s\/\\'\$\/\' \<\<\<\"${username})"\n"
 my+="password=$(echo sed \'s\/\^\'\/\/\' \<\<\<"${password}")\n"
 my+="port=$(sed 's/'${port}'/g')\n"
 my+="\n"
