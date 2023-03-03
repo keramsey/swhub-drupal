09386e1ff03beecbf0029108143abf356cb7b3ea
keramsey <kramsey@jornada-vmail.nmsu.edu>
2023-03-03 21:56:41 +0000

    Echo container id

--- src/deploy-drupal.sh
+++ src/deploy-drupal.sh
@@ -198,8 +198,9 @@ docker push ${DOCKER_ACCOUNT}/swhub-drupal-${PROJECT}:${PROJECT_TAG}
 # Determine the container id of Drupal service
 #   skip first line containing table header
 container_id=$(docker ps --filter "label=com.docker.swarm.service.name=swhub-${PROJECT}_drupal-${PROJECT}")
+echo "$container_id"
 #   only first 12 characters are the container id
-container_id=$(${container_id:0:12} | awk 'NR==2')
+container_id=${container_id:0:12} | awk 'NR==2'
 echo "$container_id"
 
 # Deploy stack
